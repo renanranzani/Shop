1dc79ebba8f86df71e8c8ad74d797b5ebd039f40
Renan Ranzani <renan.ranzani@alfaerp.com.br>
2023-10-22 15:40:21 -0300

    First commit

---
+++ Shop.Work/Shop.Work/Program.cs
@@ -0,0 +1,10 @@
+using Shop.Work;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
